80d1c7efd833e5eae6e57be2e8440cede29393fb
Jia Pan <like60955@gmail.com>
2024-09-13 17:57:13 +0800

    feat:init

---
+++ src/hook/useRequest.ts
@@ -0,0 +1,23 @@
+import Axios, { type AxiosRequestConfig, type Method } from 'axios'
+
+export const useRequest = async <R extends {}, B extends {} = {}>({
+  method,
+  url,
+  data,
+  config,
+}: {
+  method: Method
+  url: string
+  data?: B
+  config?: AxiosRequestConfig
+}) => {
+  const res = await Axios.request<R>({
+    method,
+    url,
+    data,
+    withCredentials: true,
+    ...config,
+  })
+  return res.data
+}
+
---
+++ src/service/api/useGeolocation.ts
@@ -0,0 +1,34 @@
+import { useRequest } from "@/hook/useRequest"
+
+export interface Geolocation{
+  result: {
+    type: string
+    name: string
+    crs: {
+      type: string
+      properties: {
+        name: string
+      }
+    }
+    features: Array<{
+      type: string
+      properties: {
+        TxtMemo: string
+        SHAPE_Area: number
+        分區: string
+      }
+      geometry: {
+        type: string
+        coordinates: Array<Array<Array<number>>>
+      }
+    }>
+  }
+}
+const URL = 'https://enterprise.oakmega.ai/api/v1/server/xinbei/geolocation-json?directory=tucheng.json'
+function useGeolocation(){
+  return useRequest({
+    method: 'GET',
+    url: URL,
+  })
+}
+export default useGeolocation
